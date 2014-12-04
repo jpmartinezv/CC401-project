b4360255b711b43d8926c022c1b822dc9bf0ca6a
Jason Martinez <jason1994.8@gmail.com>
2014-12-04 16:05:25 -0500

    Agregando links

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -33,7 +33,11 @@
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Canchas', 'url'=>array('/cancha/index')),
+				array('label'=>'Local', 'url'=>array('/local/index')),
+				array('label'=>'Promocion', 'url'=>array('/promocion/index')),
+				
 			),
 		)); ?>
 	</div><!-- mainmenu -->
